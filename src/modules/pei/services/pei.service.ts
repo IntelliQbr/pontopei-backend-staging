7cca0c388552daef5e7ecb057b3440401d214c92
IntelliQ <marcsbetosan@gmail.com>
2025-11-05 21:39:36 -0300

    Fix: Corrigido erro de importacao em pei.service.ts para build no Railway (v2)

--- src/modules/pei/services/pei.service.ts
+++ src/modules/pei/services/pei.service.ts
@@ -8,9 +8,9 @@ import { CreatePEIContentType } from "src/core/ai/models/types/peis/create-pei-c
 import { RenewPEIContentType } from "src/core/ai/models/types/peis/renew-pei-content.type";
 import { PEIAIService } from "src/core/ai/services/pei-ai.service";
 import { PrismaService } from "src/core/database/services/prisma.service";
-import { RequestCreatePEIDto } from "../models/create/request-create-pei.dto";
-import { RequestRenewPEIDto } from "../models/dtos/renew/request-renew-pei.dto";
-import { FindAllPEIQuery } from "../models/dtos/queries/find-all-pei.query";
+import { RequestCreatePEIDto } from "../../models/create/request-create-pei.dto";
+import { RequestRenewPEIDto } from "../../models/dtos/renew/request-renew-pei.dto";
+import { FindAllPEIQuery } from "../../models/dtos/queries/find-all-pei.query";
 import { omit } from "lodash";
 import { PEI, Prisma, ProfileRole } from "@prisma/client";
 
